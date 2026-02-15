6d5244b1971e63306e2f3d979bda2cdd0436985f
newt <51036153+newt239@users.noreply.github.com>
2026-02-16 00:21:06 +0900

    fix: disable image optimize (#56)
    * fix: disable image optimize
    
    * remove: deploy action of github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true, // Cloudflare Workers の Static Export では利用不可能
+  },
 };
 
 export default nextConfig;
