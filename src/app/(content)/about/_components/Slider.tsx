6f5a9ba1494222a63a69eda01c0152c81f27ee90
Meiryo7743 <46659204+Meiryo7743@users.noreply.github.com>
2026-02-26 19:24:12 +0900

    Squashed commit of the following:
    commit 398111c76a7043bfbc7b901c6d32b765c667cfee
    Author: Meiryo7743 <46659204+Meiryo7743@users.noreply.github.com>
    Date:   Thu Feb 26 19:23:43 2026 +0900
    
        fix: remove splider
    
    commit 09a98c3e9542992bf442a446555d92423b77b840
    Author: Meiryo7743 <46659204+Meiryo7743@users.noreply.github.com>
    Date:   Thu Feb 26 19:21:40 2026 +0900
    
        fix: build
    
    commit 29a1663f74194c70013081f32e0f35db4cc4786f
    Author: Meiryo7743 <46659204+Meiryo7743@users.noreply.github.com>
    Date:   Thu Feb 26 19:20:37 2026 +0900
    
        fix

--- src/app/(content)/about/_components/Slider.tsx
+++ src/app/(content)/about/_components/Slider.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import { Splide, SplideSlide } from "@splidejs/react-splide";
-import "@splidejs/react-splide/css";
-import { Video } from "@splidejs/splide-extension-video";
-import "@splidejs/splide-extension-video/dist/css/splide-extension-video.min.css";
-import Image from "next/image";
-
 type ImageSlide = {
   type: "image";
   src: string;
@@ -26,74 +20,5 @@ type SliderProps = {
 };
 
 export default function Slider({ slides }: SliderProps) {
-  return (
-    <div className="w-full overflow-hidden">
-      <Splide
-        options={{
-          type: "slide",
-          interval: 8000,
-          autoplay: true,
-          arrows: slides.length > 1,
-          pagination: true,
-          speed: 700,
-          video: {
-            loop: true,
-            mute: true,
-          },
-        }}
-        extensions={{ Video }}
-      >
-        {slides.map((slide, index) => {
-          if (slide.type === "image") {
-            return (
-              <SplideSlide
-                key={`${slide.src}-${index}`}
-                className="aspect-video w-full"
-              >
-                <Image
-                  fill
-                  src={slide.src}
-                  alt={slide.alt}
-                  className="aspect-video w-full object-cover"
-                />
-              </SplideSlide>
-            );
-          }
-
-          if (slide.type === "youtube") {
-            // YouTube URLから動画IDを抽出
-            const youtubeId = slide.url.match(
-              /(?:youtube\.com\/embed\/|youtu\.be\/|youtube\.com\/watch\?v=)([^&\n?#]+)/,
-            )?.[1];
-
-            if (!youtubeId) {
-              return null;
-            }
-
-            // サムネイルが提供されていない場合、YouTubeのサムネイル画像を自動取得
-            const thumbnailUrl =
-              slide.thumbnail ||
-              `https://img.youtube.com/vi/${youtubeId}/maxresdefault.jpg`;
-
-            return (
-              <SplideSlide
-                key={`${slide.url}-${index}`}
-                data-splide-youtube={`https://www.youtube.com/watch?v=${youtubeId}&autoplay=1`}
-                className="aspect-video w-full"
-              >
-                <Image
-                  fill
-                  src={thumbnailUrl}
-                  alt={slide.alt || "YouTube動画"}
-                  className="aspect-video object-cover"
-                />
-              </SplideSlide>
-            );
-          }
-
-          return null;
-        })}
-      </Splide>
-    </div>
-  );
+  return <></>;
 }
--- src/app/_components/Gallery.tsx
+++ src/app/_components/Gallery.tsx
@@ -1,57 +1,19 @@
 "use client";
 
-import { Splide, SplideSlide } from "@splidejs/react-splide";
-import "@splidejs/react-splide/css";
-import { Video } from "@splidejs/splide-extension-video";
-import "@splidejs/splide-extension-video/dist/css/splide-extension-video.min.css";
-import Image from "next/image";
-
-const slideList = [
-  {
-    src: "/img/buff_zavas.webp",
-    alt: "頭部がプロテイン飲料のザバスになった男性キャラクター2人を描いたイラスト。1人は白いワイシャツにネクタイとチョッキを着用し、腕を捲っている。もう1人は上裸でこちらを振り向きながら、筋肉を見せつけるようなガッツポーズをしている",
-  },
-  { src: "/img/credit_bread.webp", alt: "モデリングソフトで作った単位パン" },
-  {
-    src: "/img/kawaii_akizuki.webp",
-    alt: "3人の可愛らしい小さな女の子を描いたイラスト。3人とも白地にピンクやグレーのラインが入った不思議なデザインの服を着ている",
-  },
-];
-
 export default function Gallery() {
   return (
     <div className="card overflow-hidden">
-      <Splide
-        options={{
-          type: "slide",
-          interval: 3000,
-          autoplay: true,
-          arrows: false,
-          video: {
-            loop: true,
-            mute: true,
-          },
-        }}
-        extensions={{ Video }}
-      >
-        {slideList.map((item) => (
-          <SplideSlide key={item.src} className="aspect-video w-full">
-            <Image fill src={item.src} alt={item.alt} />
-          </SplideSlide>
-        ))}
-
-        <SplideSlide
-          data-splide-youtube="https://www.youtube.com/watch?v=nnJ5ml6D2UE"
-          className="aspect-video w-full"
-        >
-          <Image
-            fill
-            src="/img/pv.webp"
-            alt="デジクリ紹介PV 2024 【芝浦工業大学公認サークル】"
-            className="object-cover"
-          />
-        </SplideSlide>
-      </Splide>
+      <iframe
+        width={320}
+        height={240}
+        src="https://www.youtube.com/embed/vM_Dmc5WLxs?si=IBPqDNbc2tT4GtM2"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+        className="aspect-video w-full h-auto"
+        loading="lazy"
+        title="デジクリ紹介動画"
+      ></iframe>
     </div>
   );
 }
