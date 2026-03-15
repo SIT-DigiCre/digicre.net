9da54be023726ae35683228ea331806698308106
Meiryo7743 <46659204+Meiryo7743@users.noreply.github.com>
2026-03-15 22:42:02 +0900

    feat: NewHeader

--- src/components/NewHeader.tsx
+++ src/components/NewHeader.tsx
@@ -18,7 +18,7 @@ const Menu: React.FC<MenuProps> = ({ title, entries }) => {
 
   return (
     <nav className="flex flex-col gap-y-[32px]">
-      <p className="border-l-[4px] border-[#00b0f0] pl-[16px] text-[1.75rem] font-bold">
+      <p className="border-l-[4px] border-[#00b0f0] pl-[16px] text-[#fff] text-[1.75rem] font-bold">
         {title}
       </p>
 
@@ -31,7 +31,7 @@ const Menu: React.FC<MenuProps> = ({ title, entries }) => {
               rel={
                 isExternalLink(item.href) ? "noopener noreferrer" : undefined
               }
-              className="bg-[#404040] border-[2px] border-[#808080] p-[16px] flex gap-[16px] rounded-[16px] items-center"
+              className="bg-[#404040] border-[2px] border-[#808080] text-[#fff] p-[16px] flex gap-[16px] rounded-[16px] items-center"
             >
               <span className="w-full font-bold text-[1.25rem]">
                 {item.title}
@@ -55,84 +55,104 @@ const Menu: React.FC<MenuProps> = ({ title, entries }) => {
 
 export const NewHeader = () => {
   return (
-    <header className="bg-[#202020] text-[#fff] min-inline-[320px] px-[16px] py-[64px] flex flex-col gap-y-[64px]">
-      <DigicreLogo className="h-[64px] w-full text-white" />
+    <header>
+      <input type="checkbox" id="header-toggle" className="peer hidden" />
 
-      <Menu
-        title="デジクリについて"
-        entries={[
-          {
-            title: "ホーム",
-            href: "/",
-          },
-          {
-            title: "お知らせ",
-            href: "/news/",
-          },
-          {
-            title: "主な活動",
-            href: "/#activities",
-          },
-          {
-            title: "班紹介",
-            href: "/#teams",
-          },
-          {
-            title: "プライバシーポリシー",
-            href: "/privacy-policy/",
-          },
-          {
-            title: "入部受付フォーム",
-            href: "https://forms.gle/cY25Kc6fssqv2tZz9",
-          },
-          {
-            title: "お問い合わせ",
-            href: "mailto:contact@digicre.net",
-          },
-        ]}
-      ></Menu>
+      <label
+        htmlFor="header-toggle"
+        className="peer-checked:hidden xl:hidden fixed top-0 left-0 z-1 bg-[#00b0f0] p-[16px] rounded-br-[16px] shadow-[2px_2px_8px_0_rgba(32_32_32_/_75%)]"
+      >
+        <Icon
+          icon="material-symbols:menu-rounded"
+          className="w-[32px] h-[32px] text-[#fff]"
+        />
+      </label>
 
-      <Menu
-        title="公式アカウント"
-        entries={[
-          {
-            title: "X（Twitter）",
-            href: "https://x.com/sitdigicre",
-          },
-          {
-            title: "Instagram",
-            href: "https://www.instagram.com/digicre.shibaura/",
-          },
-          {
-            title: "YouTube",
-            href: "https://www.youtube.com/@sitdigicre",
-          },
-          {
-            title: "GitHub",
-            href: "https://github.com/SIT-DigiCre",
-          },
-        ]}
-      ></Menu>
+      <label
+        htmlFor="header-toggle"
+        className="peer-not-checked:hidden xl:hidden fixed top-0 left-0 z-1 bg-[#FF4B00] p-[16px] rounded-br-[16px] shadow-[2px_2px_8px_0_rgba(32_32_32_/_75%)]"
+      >
+        <Icon
+          icon="material-symbols:close-rounded"
+          className="w-[32px] h-[32px] text-[#fff]"
+        />
+      </label>
 
-      <Menu
-        title="関連サイト"
-        entries={[
-          {
-            title: "芝浦工業大学",
-            href: "https://www.shibaura-it.ac.jp/",
-          },
-        ]}
-      ></Menu>
+      <div className="max-md:peer-not-checked:hidden fixed top-0 left-0 z-0 overscroll-none bg-[#202020] min-inline-[320px] h-full overflow-y-auto px-[16px] py-[64px] flex flex-col gap-y-[64px]">
+        <DigicreLogo className="aspect-[176_/_48] w-auto h-[64px] shrink-0 text-[#fff]" />
 
-      <Menu
-        title="デジクリ部員向け"
-        entries={[
-          {
-            title: "デジコア",
-            href: "https://core3.digicre.net/",
-          },
-        ]}
-      ></Menu>
+        <Menu
+          title="デジクリについて"
+          entries={[
+            {
+              title: "ホーム",
+              href: "/",
+            },
+            {
+              title: "主な活動",
+              href: "/#activities",
+            },
+            {
+              title: "班紹介",
+              href: "/#teams",
+            },
+            {
+              title: "プライバシーポリシー",
+              href: "/privacy-policy/",
+            },
+            {
+              title: "入部受付フォーム",
+              href: "https://forms.gle/cY25Kc6fssqv2tZz9",
+            },
+            {
+              title: "お問い合わせ",
+              href: "mailto:contact@digicre.net",
+            },
+          ]}
+        ></Menu>
+
+        <Menu
+          title="公式アカウント"
+          entries={[
+            {
+              title: "X（Twitter）",
+              href: "https://x.com/sitdigicre",
+            },
+            {
+              title: "Instagram",
+              href: "https://www.instagram.com/digicre.shibaura/",
+            },
+            {
+              title: "YouTube",
+              href: "https://www.youtube.com/@sitdigicre",
+            },
+            {
+              title: "GitHub",
+              href: "https://github.com/SIT-DigiCre",
+            },
+          ]}
+        ></Menu>
+
+        <Menu
+          title="関連サイト"
+          entries={[
+            {
+              title: "芝浦工業大学",
+              href: "https://www.shibaura-it.ac.jp/",
+            },
+          ]}
+        ></Menu>
+
+        <Menu
+          title="デジクリ部員向け"
+          entries={[
+            {
+              title: "デジコア",
+              href: "https://core3.digicre.net/",
+            },
+          ]}
+        ></Menu>
+      </div>
     </header>
   );
 };
